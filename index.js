4234d7d5dcc3565dcf06df73a383dc083a8ffb9f
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-08-23 19:47:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const leastFavoriteCustomer = 'not Charity';
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob';
+} 
